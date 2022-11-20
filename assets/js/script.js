20c304c698a031d05f36ae0d92d3969b5dc92270
lucaslagant <lucaslagant@gmail.com>
2022-11-21 00:40:45 +0100

    sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const menuToggle = document.querySelector('.toggle');
+const sidebar = document.querySelector('.sidebar');
 
 menuToggle.addEventListener("click", () =>{
     menuToggle.classList.toggle('active');
+    sidebar.classList.toggle('active');
 });
 
 
